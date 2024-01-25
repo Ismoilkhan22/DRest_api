ab3e81d61ebbe7291caa25e4dea816a65cad9fd3
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-01-25 11:15:11 +0500

    2
    post va get zaprostlarida ma'lumot ni  json  formatda chiqarishni ko'rdik

--- women/models.py
+++ women/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Women(models.Model):
     title = models.CharField(max_length=256)
-    content = models.TextField(blank=True)
+    contents = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
--- women/views.py
+++ women/views.py
@@ -1,11 +1,24 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework.response import Response
 
 from women.models import Women
 from women.serializers import WomenSerializer
 
 
 class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    def get(self, request):
+        lst = Women.objects.all().values()
+
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            contents=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
 
 
